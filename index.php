55830734297a55da8efe7cdd39709f3846b5c9e4
Jimmy419 <suifeng419@126.com>
2018-02-06 22:25:01 +0800

    阿道夫

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+		$name = "jimmy";
+		echo "<title>{$name}'s lamp {\$name} project</title>";
+	?>
+</head>
+<body>
+<div style="background: red; height: 100px;"></div>
+</body>
+</html>
