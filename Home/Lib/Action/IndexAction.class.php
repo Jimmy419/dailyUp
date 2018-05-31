29b0d3355f4336eaafccde77e4eda948acb53f98
Jimmy419 <suifeng419@126.com>
2018-05-31 22:50:24 +0800

    成型

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -6,10 +6,10 @@ class IndexAction extends CommonAction {
 		$this->colums = $cols->where('uid='.$_SESSION['uid'])->select(); 
 		
 		$utbs = M("utbs");
-		$this->tables = $utbs->where('uid='.$_SESSION['uid'])->select();
-		// echo $class->getLastSql();
+		$this->tables = $utbs->field('utbs.*,tconf.cid,columns.mclass,columns.name,maindata.value,maindata.notes,maindata.time,maindata.ctime')->join(array('LEFT JOIN tconf ON utbs.id = tconf.tid','LEFT JOIN columns ON tconf.cid = columns.id','LEFT JOIN maindata ON columns.id = maindata.cid'))->where('utbs.id='.$_SESSION['uid'])->select();
+		// echo $utbs->getLastSql();
 		// echo "<pre>";
-		// print_r($this->colums);
+		// print_r($this->tables);
 		// echo "</pre>";
 		$data = M("maindata");
 		$this->datas = $data->where('uid='.$_SESSION['uid'])->select();
