79c01a7e6a9a4be224a52a379275c9b9b2bf776e
Jimmy419 <suifeng419@126.com>
2018-06-26 23:14:20 +0800

    chart redraw

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -2,32 +2,31 @@
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends CommonAction {
     public function index(){
-		$utbs = M("utbs");
-        $_SESSION['tabidx'] = 1;
-		$tables = $utbs->field('utbs.*,tconf.cid,columns.mclass,columns.name,maindata.value,maindata.id as dataId,maindata.notes,maindata.time,maindata.ctime')->join(array('LEFT JOIN tconf ON utbs.id = tconf.tid','LEFT JOIN columns ON tconf.cid = columns.id','LEFT JOIN maindata ON columns.id = maindata.cid'))->where('utbs.id='.$_SESSION['utb'][id])->order('columns.id,maindata.time')->select();
-        foreach ($tables as $k => $v) {
-            $v[time] = date('Y-m-d', $v[time]);
-            $tables[$k][time] =$v[time];
-        }
-        $this->tables = $tables;
-        $this->tblist = $utbs->where('uid='.$_SESSION['uid'])->select();
-
+		// $utbs = M("utbs");
+  //       $_SESSION['tabidx'] = 1;
+		// $tables = $utbs->field('utbs.*,tconf.cid,columns.mclass,columns.name,maindata.value,maindata.id as dataId,maindata.notes,maindata.time,maindata.ctime')->join(array('LEFT JOIN tconf ON utbs.id = tconf.tid','LEFT JOIN columns ON tconf.cid = columns.id','LEFT JOIN maindata ON columns.id = maindata.cid'))->where('utbs.id='.$_SESSION['utb'][id])->order('columns.id,maindata.time')->select();
+  //       foreach ($tables as $k => $v) {
+  //           $v[time] = date('Y-m-d', $v[time]);
+  //           $tables[$k][time] =$v[time];
+  //       }
+  //       $this->tables = $tables;
+  //       $this->tblist = $utbs->where('uid='.$_SESSION['uid'])->select();
 		$this->display();
     }
 
     public function getDate(){
         $utbs = M("utbs");
         $tconf = M("tconf");
+        $subtbs = M("subtbs");
         $tables = $utbs->field('utbs.id as tid,utbs.uid,utbs.tablename,tconf.cid,columns.mclass,columns.name,maindata.value,maindata.id as dataId,maindata.notes,maindata.time,maindata.ctime')->join(array('LEFT JOIN tconf ON utbs.id = tconf.tid','LEFT JOIN columns ON tconf.cid = columns.id','LEFT JOIN maindata ON columns.id = maindata.cid'))->where('utbs.id='.$_SESSION['utb'][id])->order('columns.id,maindata.time')->select();
-        // $tblist = $utbs->where('uid='.$_SESSION['uid'])->select();
         $tableCols = $tconf->field('tconf.cid,columns.name')->join(array('LEFT JOIN columns ON tconf.cid = columns.id'))->where('tconf.tid='.$_SESSION['utb'][id])->select();
-        $subtables = $utbs->field('utbs.id as tid,subtbs.name,stbcols.subtbcolname,stbcols.calculation,stbcols.unit')->join(array('LEFT JOIN subtbs ON utbs.id = subtbs.tid','LEFT JOIN stbcols ON stbcols.stid = subtbs.id'))->where('utbs.id='.$_SESSION['utb'][id])->select();
+        $subtables = $subtbs->field('subtbs.id as stid,subtbs.name,stbcols.subtbcolname,stbcols.calculation,stbcols.unit')->join(array('LEFT JOIN stbcols ON stbcols.stid = subtbs.id'))->where('subtbs.tid='.$_SESSION['utb'][id])->select();
         $data['table'] = $_SESSION['utb'];
-        $data['tables'] = $tblist;
         $data['tablecols'] = $tableCols;
         $data['data'] = $tables;
-        $data['subtables'] = $subtables;
+        $data['subcolumns'] = $subtables;
         $data['status']  = 1;
+        $data['subtbs'] = $subtbs->where('tid='.$_SESSION['utb'][id])->select();
         $this->ajaxReturn($data);  
     }
 
--- Home/Tpl/Public/js/index.index.js
+++ Home/Tpl/Public/js/index.index.js
@@ -9,11 +9,11 @@ $(function() {
             console.log(result);
             var data = result.data;
             var columns = result.tablecols;
-            var tables = result.tables;
             var table = result.table;
-            var subtabs = result.subtables;
-            // subCharts(tables,data,subtabs);
+            var subtabs = result.subtbs;
+            var subtbcols = result.subcolumns;
             drawCharts(table,columns,data);
+            subCharts(subtabs,subtbcols,data);
         }
     })
 
@@ -40,7 +40,8 @@ $(function() {
 
         Highcharts.chart('chart'+table.id, {
             chart: {
-                type: 'spline'
+                type: 'spline',
+                zoomType: 'x'
             },
             title: {
                 text: table.tablename
@@ -77,133 +78,90 @@ $(function() {
         });
     }
 
-    function clarrifySubCols(table,subtables){
-        var tempSubTbs = _.filter(subtables, function(num){ return num.tid == table.id; });
-        var sbtbnames = _.uniq(_.pluck(tempSubTbs, 'name'));
-        var finalsubtables = [];
-        sbtbnames.forEach(function(sbtbname){
-            var subtableItem = {
-                'name': sbtbname,
-                'columns': []
-            }
-            tempSubTbs.forEach(function(subtable){
-                if(subtable.name === sbtbname){
-                    subtableItem.columns.push(subtable)
-                }
-            });
-            finalsubtables.push(subtableItem);
+    function subCharts(subtables,subtablecols,sourcedata){
+
+        //get table dates;
+        var tableDates = null;
+        var tempDates = [];
+        sourcedata.forEach(function(sditem){
+            tempDates.push(parseFloat(sditem.time) * 1000);
         });
-        return finalsubtables;
-    }
+        tableDates = _.uniq(tempDates).sort();
 
-    function subCharts(tables,sourcedata,subtabs){
-        // tables:[{id:4,uid:1,tablename:'Stock'}]
-        // sourcedata:[{cid:"20",ctime:"1528515614",dataId:"974",tid:"4",mclass:"Stock",name:"Stock Value",notes:null,tablename:"Stock",time:"1516147200",uid:"1",value:"35903.5300"}]
-
-        tables.forEach(function(table){
-            var tableSourceData = [];
-            var tableDates = null;
-            var tableColumns = [];
-            var subTableData = [];
-            var subtables = clarrifySubCols(table,subtabs);
-
-            //get table data;
+        //clarrify soucedata
+        var dataForCalc = [];
+        tableDates.forEach(function(dateItem){
+            var subTableItem = {
+                date: dateItem
+            }
             sourcedata.forEach(function(sditem){
-                if(sditem.tid === table.id){
-                    tableSourceData.push(sditem);
+                if(parseFloat(sditem.time) * 1000 === dateItem){
+                    subTableItem[sditem.cid] = sditem.value;
                 }
             });
+            dataForCalc.push(subTableItem);
+        });
 
-            //get table dates;
-            var tempDates = [];
-            var columnsTemp = [];
-            tableSourceData.forEach(function(sditem){
-                tempDates.push(parseFloat(sditem.time) * 1000);
-                var sditemColumn = {
-                    cid: sditem.cid,
-                    name: sditem.name
-                }
-                columnsTemp.push(sditemColumn);
-            });
-            tableDates = _.uniq(tempDates).sort();
-
-            //get columns;
-            columnsTemp.forEach(function(tempColItem){
-                var foundCol = _.find(tableColumns, function(col){ return col.cid === tempColItem.cid; })
-                if(!foundCol){
-                    tableColumns.push(tempColItem);
-                }
-            });
-
-            //arrange data for subtable
-            tableDates.forEach(function(dateItem){
-                var subTableItem = {
-                    date: dateItem
-                }
-                tableSourceData.forEach(function(sdItem){
-                    if(parseFloat(sdItem.time) * 1000 === dateItem){
-                        subTableItem[sdItem.cid] = sdItem.value;
-                    }
-                });
-                subTableData.push(subTableItem);
-            });
-
-            //calculate sub table data
-            subtables.forEach(function(subtable, subtableIx){
-                subtable.chartData = [];
-                subtable.columns.forEach(function(subCol){
-                    var tempdata = [];
-                    subTableData.forEach(function(subDataItem){
-                        var calc = subCol.calculation.replace(/(\[[0-9]+\])/g,"subDataItem$1");
+        subtables.forEach(function(subtable){
+            subtable.data = [];
+            subtablecols.forEach(function(subtablecol){
+                if(subtablecol.stid == subtable.id){
+                    var tempdata = {
+                        "name": subtablecol.subtbcolname,
+                        "data": []
+                    };
+                    dataForCalc.forEach(function(calcitem){
+                        var calc = subtablecol.calculation.replace(/(\[[0-9]+\])/g,"calcitem$1");
                         var calculatedVal = eval(calc);
+                        var dataforchart = [
+                            calcitem.date,
+                            calculatedVal
+                        ];
                         if(!isNaN(calculatedVal)){
-                            tempdata.push([subDataItem.date, calculatedVal]);
-                        };
-                    })
-                    subtable.chartData.push({
-                        name: subCol.subTbColName,
-                        data: tempdata
-                    })
-                })
-                
+                            tempdata.data.push(dataforchart);
+                        }
+                    });
+                    subtable.data.push(tempdata);
+                }
+            })
 
-                $('.for_sub_chart').append('<div id="subtable'+subtableIx+'"></div>');
-                var chart1 = Highcharts.chart('subtable'+subtableIx, {
-                    chart: {
-                        type: 'spline'
-                    },
+            $('.chart-box').append('<div class="col-sm-6" id="subtable'+subtable.id+'"></div>');
+            Highcharts.chart('subtable'+subtable.id, {
+                chart: {
+                    type: 'spline',
+                    zoomType: 'x'
+                },
+                title: {
+                    text: subtable.name
+                },
+                subtitle: {
+                    text: null
+                },
+                xAxis: {
                     title: {
-                        text: subtable.name
-                    },
-                    subtitle: {
                         text: null
                     },
-                    xAxis: {
-                        title: {
-                            text: null
-                        },
-                        type: "datetime",
-                        labels: {
-                            format: '{value: %Y%m%d}'
+                    type: "datetime",
+                    labels: {
+                        format: '{value: %Y%m%d}'
+                    }
+                },
+                yAxis: {
+                    title: {
+                        text: null
+                    }
+                },
+                tooltip: {
+                    split: true
+                },
+                plotOptions: {
+                    spline: {
+                        marker: {
+                            enabled: true
                         }
-                    },
-                    yAxis: {
-                        title: {
-                            text: null
-                        }
-                    },
-                    tooltip: {
-                        split: true
-                    },
-                    plotOptions: {
-                        spline: {
-                            marker: {
-                                enabled: true
-                            }
-                        }
-                    },
-                    series: subtable.chartData
-                });
+                    }
+                },
+                series: subtable.data
             });
         })
     }    
